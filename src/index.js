371adf21f5c2d7682a75517fb21648691610d564
IgorDive <ihar.kazlovich@gmail.com>
2018-03-15 20:11:02 +0300

    modified conditions src/index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
 module.exports = function count(s, pairs) {
   let n = 0;
   let valueMassive = [];
+  let sArr = s.split('').map( elem => +elem );
 
   if ( pairs.some( arr => arr[1] > 30 ) ) return;
   n = pairs.map( (arr) => Math.pow( arr[0], arr[1] ) ).reduce( (prevValue, item) => prevValue * item );
   if ( n > 50000 ) return; 
-
-  for ( let i = 1; i <= n; i++ ) {
-    for ( let j = 0; j < s.length; j++ ) {
-      if ( s[j] && !checkGreatestComDivIsNotOne( i + j ) ) {
+  
+  for ( let i = 0; i <= n; i++ ) {
+    for ( let j = 0; j < sArr.length; j++ ) {
+      if ( sArr[j] && !checkGreatestComDivIsNotOne( i + j ) && (i + j) ) {
         addValueToMassive(i);
       };
-      if ( !s[j] && checkGreatestComDivIsNotOne( i + j ) ) {
+      if ( !sArr[j] && checkGreatestComDivIsNotOne( i + j ) && (i + j) ) {
         addValueToMassive(i);
       };
     };
   };
   
   function checkGreatestComDivIsNotOne( sum ) {
-    for ( l = 0; l < pairs.length; l++ ) {
-      if ( sum / pairs[l][0] < 2 ) continue;
+   
+    for ( let l = 0; l < pairs.length; l++ ) {
       if ( !(sum % pairs[l][0]) ) return true; 
     }
     
